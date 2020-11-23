91b11b7d1e40393cb69054a754807bf054f61ee2
MAussem <Aussem.Maxwell@gmail.com(home)>
2020-11-22 21:09:01 -0500

    no more smooth scroll

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
 window.onload = () => {
     setTimeout(() => {
         document.querySelector("body").classList.add
-        ("display");
+            ("display");
     }, 2000);
 };
 
@@ -11,37 +11,7 @@ document.querySelector(".hamburger-menu").addEventListener("click", () => {
 });
 
 
-document.querySelector(".scroll-btn").addEventListener
-("click", () => {
-document.querySelector("html").style.scrollBehavior = "smooth";
-setTimeout(()=>{
-        document.querySelector("html").style.scrollBehavior = "unset";
-    }, 1000);
-});
 
-
-const carouselSlide = document.querySelector('.carousel-slide');
-const carouselImages = document.querySelectorAll('.carousel-slide img');
-
-// Buttons
-
-const prevBtn = document.querySelector('#prevBtn');
-const nextBtn = document.querySelector('#nextBtn');
-
-// Counter
-
-let counter = 1;
-const size = carouselImages[0].clientWidth
-
-carouselSlide.style.transform = 'translateX(' + (-size * counter) + 'px)';
-
-// Button Listeners 
-
-nextBtn.addEventListener('click', () => {
-    carouselSlide.style.transition = "transform 0.4s ease-in-out";
-    counter++;
-    carouselSlide.style.transform = 'translateX(' + (-size * counter) + 'px)';
-});
 
 
 
